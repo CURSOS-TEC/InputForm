06c4a9cf5cf0cf72fa56e6bb9109ef97fe58a753
Juan Esteban Navarro Camacho <jnavcamacho@gmail.com>
2018-08-23 17:30:41 -0600

    stable version

--- app/src/main/java/com/soa4id/tec/inputform/MainActivity.java
+++ app/src/main/java/com/soa4id/tec/inputform/MainActivity.java
@@ -1,7 +1,9 @@
 package com.soa4id.tec.inputform;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -33,6 +35,10 @@ public class MainActivity extends AppCompatActivity {
                         mEmailInput.getText().toString(),
                         mAgeInput.getText().toString(),
                         mPhoneInput.getText().toString());
+
+                Intent intent = new Intent(getApplicationContext(),ProfileActivity.class);
+                startActivity(intent);
+                Log.i("SOAUSER", mUser.getmFullName());
             }
         });
     }
